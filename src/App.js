306f02b6bcef5e7b69fda8b8c4b2cf66a71f39c0
ShohruxMurodovich <murodovichshohruh@gmail.com>
2022-05-03 16:50:09 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import wind from "./Assets/Images/wind.png";
+import wet from "./Assets/Images/wet.png";
+import umbrella from "./Assets/Images/umbrella.png";
+import Images from "./Helper/Images"
+import Loader from "./Assets/Images/loader.gif"
+
+
+
+import "./Assets/Main.css"
 
 function App() {
+
+  const APIKEY = "5b6cead7343ca43653b06278c6564ff0"
+  const [city, setCity] = useState("Tashkent")
+  const [data, setData] = useState({
+    isFetched: false,
+    data: {},
+    error: false,
+  })
+  const [unit , setUnit] = useState("metric")
+
+  useEffect(() => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=${unit}`)
+      .then((res) => res.json())
+      .then((data) => setData({
+        isFetched: true,
+        data: data,
+        error: false,
+      }))
+  }, [city , unit])
+
+  const handleSearch = (evt) => {
+    if (evt.code === "Enter") {
+      setCity(evt.target.value)
+      evt.target.value = null
+    }
+  }
+
+  console.log(data.data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <h2 className='title'>Right now in <input className='input' onKeyUp={handleSearch} type="text" />.</h2>
+
+      {
+        data.isFetched ? (
+          <case>
+            <h2 className='city-name'>{data.data.name}</h2>
+
+            <div className="inner-case">
+
+              <div className="wrapper">
+                <Images code={data.data.weather[0].id}/>
+                <p className='city-temp'>{Math.round(data.data.main.temp)} °C</p>
+              </div>
+
+              <div className="wind">
+
+                <div className="wind-case">
+                  <img className="wind-icon" src={wind} alt="Wind icon" />
+                  <p className="wind-text">{data.data.wind.speed}.mph</p>
+                </div>
+
+                <div className="wind-case">
+                  <img className="wind-icon" src={wet} alt="wet icon" />
+                  <p className="wind-text">{data.data.main.humidity} %</p>
+                </div>
+
+                <div className="wind-case">
+                  <img className="wind-icon" src={umbrella} alt="umbrella icon" />
+                  <p className="wind-text">{data.data.clouds.all} %</p>
+                </div>
+
+              </div>
+
+            </div>
+
+          <div className="btn-case">
+            <button className='btn metric' type='button' onClick={() => setUnit("metric")}>°C</button>
+            <button className='btn standard' type='button' onClick={() => setUnit("standard")}>℉</button>
+            </div>
+
+          </case>
+        ) : (
+          <img className='loader' src={Loader} alt='Loader'/>
+        )
+      }
+
+
     </div>
   );
 }
---
+++ src/Helper/Images.js
@@ -0,0 +1,36 @@
+import Storm from "../Assets/Images/storm.gif"
+import Drizzle from "../Assets/Images/drizzle.gif"
+import Rain from "../Assets/Images/rain.gif"
+import Snow from "../Assets/Images/snow.gif"
+import Atmosphere from "../Assets/Images/atmosphere.gif"
+import Day from "../Assets/Images/day.gif"
+import Cloud from "../Assets/Images/cloud.gif"
+
+
+
+
+const Images = ({code}) =>{
+  if(code >= 200 && code<=232){
+    return <img src={Storm} alt="Icon"></img>
+  }
+  if(code >= 300 && code<=321){
+    return <img src={Drizzle} alt="Icon"></img>
+  }
+  if(code >= 500 && code<=531){
+    return <img src={Rain} alt="Icon"></img>
+  }
+  if(code >= 600 && code<=622){
+    return <img src={Snow} alt="Icon"></img>
+  }
+  if(code >= 700 && code <=781){
+    return <img src={Atmosphere} alt="Icon"></img>
+  }
+  if(code == 800){
+    return <img src={Day} alt="Icon"></img>
+  }
+  if(code >= 801 && code<=804){
+    return <img src={Cloud} alt="Icon"></img>
+  }
+}
+
+export default Images;
